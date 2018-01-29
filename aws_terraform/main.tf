b389e1c41a6d764105af481a88c38c1c34230eea
mygit676 <mydevops676@gmail.com>
2018-01-29 15:24:27 -0500

    Update main.tf

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   backend "s3" {}
 }
@@ -6,10 +5,9 @@ terraform {
 provider "aws" {}
 
 resource "aws_instance" "example"  {
-        ami = ""
+        ami = "ami-97785bed"
         instance_type = "t2.micro"
         tags {
         Name="terraform-instance"
         }
 }
-
